8039094705f9328938ebf919e65687524c05e2f5
wooldridge12 <wooldridgemikey@gmail.com>
2022-06-28 15:08:48 -0400

    Created the buttons to link to funFacts or care pages.

--- src/components/LearnMore/LearnMore.js
+++ src/components/LearnMore/LearnMore.js
@@ -1,10 +1,22 @@
 import React from "react"
+import { useHistory } from "react-router-dom"
 import "./LearnMore.css"
 
 
 // This will just return facts about bearded dragon care.
 export const LearnMorePage = () => {
+    const history = useHistory()
     return (
+        <div className="factsChoices">
         <div>Bearded Dragon!!!</div>
+
+        <button onClick={() => {
+            history.push({pathname:"/care"})
+        }}>Care</button>
+
+        <button onClick={() => {
+            history.push({pathname:"/funFacts"})
+        }}>Fun Facts</button>
+        </div>
     )
 }
